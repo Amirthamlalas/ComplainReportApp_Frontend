772983f9bcab9c5067c15b192454114fc1f70c34
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-09 16:05:24 +0530

    added admin

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent {
+
+  username=""
+  password=""
+
+  readValue=()=>{
+    let data:any={"username":this.username,"password":this.password}
+    console.log(data)
+  }
+}
