5141d010f03d6161bd572c1c250ff16e52223ab4
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-10 21:07:22 +0530

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,39 @@ import { AdminComponent } from './admin/admin.component';
 import { FormsModule } from '@angular/forms';
 import { UserSignupComponent } from './user-signup/user-signup.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
+const myroute:Routes=[
+  {
+    path:"",
+    component:AdminComponent
+  },
+  {
+    path:"userlogin",
+    component:UserLoginComponent
+  },
+  {
+    path:"usersignup",
+    component:UserSignupComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     UserSignupComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
